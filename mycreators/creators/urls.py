b50c094725cf1473eb9b63480a51e97f705df6c8
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-07-09 16:00:06 +0200

    app: creator - Added a detailview of the creators page
    Set up urls,views and the html to be able to click the creators name and land on their about me page

--- mycreators/creators/urls.py
+++ mycreators/creators/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreatorListView
+from .views import CreatorListView, CreatorAboutMeView
 
 urlpatterns = [
     path('', CreatorListView.as_view(), name='creator_list'),
+    path('<int:pk>/', CreatorAboutMeView.as_view(), name='creator_aboutme'),
 ]
--- mycreators/creators/views.py
+++ mycreators/creators/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Creator
 from django.db.models import Q
 
 
 # Create your views here.
 
+#=============== List of creators ===============
 class CreatorListView(ListView):
     model = Creator
     template_name = 'creators/creator_list.html'
@@ -21,3 +22,14 @@ class CreatorListView(ListView):
                 Q(about_me__icontains=search_query)
             )
         return queryset
+
+#=============== Creators about me page =================
+class CreatorAboutMeView(DetailView):
+    model = Creator
+    template_name = 'creators/creator_aboutme.html'
+    context_object_name = 'creator'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = self.object.posts.all().order_by('-created_at')
+        return context

5353645db71e2986497e9b90fd0e8dc2ab46d647
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-07-09 14:40:24 +0200

    app: creators - set up admin.py

--- mycreators/creators/admin.py
+++ mycreators/creators/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Creator, Post
 
 # Register your models here.
+
+@admin.register(Creator)
+class CreatorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'about_me')
+    search_fields = ('user__username', 'about_me')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('creator', 'content', 'created_at', 'updated_at')
+    list_filter = ('creator', 'created_at')
+    search_fields = ('creator__user__username', 'content')
+    readonly_fields = ('created_at', 'updated_at')

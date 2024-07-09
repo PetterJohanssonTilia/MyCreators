8459fb2d0c4fc6abe7ac4651482831ba6bc0828b
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-07-09 15:48:14 +0200

    app: creators - added a search bar to search creators
    You can search username or words from their about me description

--- mycreators/creators/views.py
+++ mycreators/creators/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Creator
+from django.db.models import Q
+
 
 # Create your views here.
 
@@ -8,3 +10,14 @@ class CreatorListView(ListView):
     model = Creator
     template_name = 'creators/creator_list.html'
     context_object_name = 'creators'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('search')
+        #Search creators with username and about me information
+        if search_query:
+            queryset = queryset.filter(
+                Q(user__username__icontains=search_query) |
+                Q(about_me__icontains=search_query)
+            )
+        return queryset

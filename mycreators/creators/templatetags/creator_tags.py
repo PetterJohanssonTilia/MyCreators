4272f2be946c374b1b5dac0d8396dec30caec4f5
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-07-10 14:12:17 +0200

    build: base.html - Creator status
    Now you get "become a creator" if you haven't submitted to become one. Once you've submitted you'll see "creator status: x" depending on your status

--- mycreators/creators/templatetags/creator_tags.py
+++ mycreators/creators/templatetags/creator_tags.py
@@ -3,6 +3,10 @@ from creators.models import Creator
 
 register = template.Library()
 
-@register.filter
-def is_creator(user):
-    return Creator.objects.filter(user=user).exists()
+@register.simple_tag
+def creator_status(user):
+    try:
+        creator = Creator.objects.get(user=user)
+        return creator.status
+    except Creator.DoesNotExist:
+        return None

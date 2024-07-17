1048a04e3a9cd343c90271646dbc7b1a691070af
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-07-17 10:55:24 +0200

    bugfix: moved the creator_status check inside the is_authenticated check
    I also imported Anonymoususer from django.contrib.auth.models to return None if isinstance AnonymousUser

--- mycreators/creators/templatetags/creator_tags.py
+++ mycreators/creators/templatetags/creator_tags.py
@@ -1,10 +1,13 @@
 from django import template
+from django.contrib.auth.models import AnonymousUser
 from creators.models import Creator
 
 register = template.Library()
 
 @register.simple_tag
 def creator_status(user):
+    if isinstance(user, AnonymousUser):
+        return None
     try:
         creator = Creator.objects.get(user=user)
         return creator.status
